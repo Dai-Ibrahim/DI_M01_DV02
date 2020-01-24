4f2f85442b529c4b112474cd0626b825f7f84559
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-01-23 20:25:50 -0700

    Update Align.cs

--- Assets/Scripts/Align.cs
+++ Assets/Scripts/Align.cs
@@ -8,12 +8,11 @@ public class Align
     public Kinematic myBoi;
 
     float maxAngularAcceleration = 100f; // 5
-    float maxRotation = 45f; // maxAngularVelocity
+    float maxRotation = 45f; 
 
-    //Set radius where myBoi starts to slow down
     float slowRadius = 10f;
 
-    float timeToTarget = 0.1f;//Get up to speed time
+    float timeToTarget = 0.1f;
 
    
     public virtual float getMyBoiAngle()
@@ -25,34 +24,31 @@ public class Align
     {
         SteeringOutput result = new SteeringOutput();
 
-        // get the naive direction to the myBoi
-        //float rotation = Mathf.DeltaAngle(selectedBoi.transform.eulerAngles.y, myBoi.transform.eulerAngles.y);
+       
         float rotation = Mathf.DeltaAngle(selectedBoi.transform.eulerAngles.y, getMyBoiAngle());
         float rotationSize = Mathf.Abs(rotation);
 
 
-        // if we are outside the slow radius, then use maximum rotation
         float myBoiRotation = 0.0f;
         if (rotationSize > slowRadius)
         {
             myBoiRotation = maxRotation;
         }
-        else // otherwise use a scaled rotation
+        else 
         {
             myBoiRotation = maxRotation * rotationSize / slowRadius;
         }
 
-        // the final myBoiRotation combines speed (already in the variable) and direction
+        
         myBoiRotation *= rotation / rotationSize;
 
-        // acceleration tries to get to the myBoi rotation
-        // something is breaking my angularVelocty... check if NaN and use 0 if so
+      
         float currentAngularVelocity = float.IsNaN(selectedBoi.angularVelocity) ? 0f : selectedBoi.angularVelocity;
         result.angularVelocity= myBoiRotation - currentAngularVelocity;
         result.angularVelocity/= timeToTarget;
 
         float angularAcceleration = Mathf.Abs(result.angularVelocity);
-        if (angularAcceleration > maxAngularAcceleration) //is acceleration going over the max
+        if (angularAcceleration > maxAngularAcceleration) 
         {
             result.angularVelocity/= angularAcceleration;
             result.angularVelocity*= maxAngularAcceleration;
